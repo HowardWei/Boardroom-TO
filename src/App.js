856c1f96264d59234a0c66c8f6335a13dc61fe7c
mbeylin <mark_beylin@hotmail.com>
2016-09-27 13:17:32 -0400

    added proposals list component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react'
 import './App.css'
 
 import AccountListContainer from 'components/AccountList/AccountListContainer'
+import ProposalsListContainer from 'components/ProposalsList/ProposalsListContainer'
+
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <AccountListContainer web3={this.props.web3} />
+      <ProposalsListContainer web3={this.props.web3} />
+      <AccountListContainer web3={this.props.web3} />
       </div>
     )
   }
