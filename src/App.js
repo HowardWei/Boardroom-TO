7167e04c485703dda7581721215045af3801a07d
mbeylin <mark_beylin@hotmail.com>
2016-09-28 14:59:02 -0400

    added logo

--- src/App.js
+++ src/App.js
@@ -10,6 +10,9 @@ class App extends Component {
   render () {
     return (
       <div className="App">
+      <div style={{width: '100%', display: 'block'}}>
+        <img src='../logo.svg' style={{margin: '0 auto', display: 'block', height: '135px'}}></img>
+      </div>
       <ProposalsListContainer web3={this.props.web3} />
       <MembersList web3={this.props.web3} />
       </div>
